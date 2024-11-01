a91b6796cd60b84944662b50e2da35b6f6e82d12
ZiadGaballah <124694216+ZiadGaballah@users.noreply.github.com>
2024-11-01 13:10:41 +0200

    Update circ_4.v

--- test/InputFiles/circ_4.v
+++ test/InputFiles/circ_4.v
@@ -8,7 +8,7 @@ output o;    // declaring output
 wire w1;
 wire w2;     // declaring internal wires
 
-  nand (w1, a, b); // (ab)'
-  or (w2, w1, b); // (ab)' + b
-  nor (o, c, w2); // o = (c + (ab)' + b)'
+  nand g0 (w1, a, b); // (ab)'
+  or g1 (w2, w1, b); // (ab)' + b
+  nor g2 (o, c, w2); // o = (c + (ab)' + b)'
   

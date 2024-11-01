d9859718dedbe221e1298f95d870f38f9ff150b3
ZiadGaballah <124694216+ZiadGaballah@users.noreply.github.com>
2024-11-01 12:34:06 +0200

    Create Circ_1.v

---
+++ test/InputFiles/Circ_1.v
@@ -0,0 +1,13 @@
+module circ_1(a, b, c, o);
+input a;
+input b;
+input c;     // declaring inputs
+
+output o;    // declaring output
+
+wire w1;
+wire w2;     // declaring internal wires
+
+  not g0 (w1, a);  // a'
+  and g1 (w2, b, c);  // bc
+  or g2 (o, w1, w2);  // o = a'+bc
